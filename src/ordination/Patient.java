9bb32ee079686912f67f7076a722d96ca6f95315
Marcus Husted <Marcusaj.husted@gmail.com>
2024-10-01 09:46:20 +0200

    Patient class

--- src/ordination/Patient.java
+++ src/ordination/Patient.java
@@ -1,9 +1,13 @@
 package ordination;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Patient {
     private final String cprNr;
     private final String navn;
     private final double vægt;
+    private final List<Ordination> ordinations = new ArrayList<>();
 
     public Patient(String cprNr, String navn, double vægt) {
         this.cprNr = cprNr;
@@ -18,5 +22,17 @@ public class Patient {
     @Override
     public String toString() {
         return navn + "  " + cprNr;
+    }
+
+    public void addOrdination(Ordination ordination) {
+        this.ordinations.add(ordination);
+    }
+
+    public void removeOrdination(Ordination ordination) {
+        this.ordinations.remove(ordination);
+    }
+
+    public List<Ordination> getOrdinationer() {
+        return ordinations;
     }
 }
