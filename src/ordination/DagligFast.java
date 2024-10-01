2af1e0aa67707d944f06c430de1f55673f2f3ba6
Phidk <Phidkalt@gmail.com>
2024-10-01 11:02:36 +0200

    DagligFast

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -1,12 +1,51 @@
 package ordination;
 
 import java.lang.reflect.Array;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
 
-public class DagligFast {
+public class DagligFast extends Ordination{
     private final Dosis[] doser = new Dosis[4];
 
+    public DagligFast(LocalDate startDato, LocalDate slutDato, Patient patient,
+                      double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
+
+        super(startDato, slutDato, patient);
+
+        doser[0] = new Dosis(LocalTime.of(8, 00), morgenAntal);
+        doser[1] = new Dosis(LocalTime.of(12, 00), middagAntal);
+        doser[2] = new Dosis(LocalTime.of(18, 00), aftenAntal);
+        doser[3] = new Dosis(LocalTime.of(00, 00), natAntal);
+    }
+
+    @Override
+    public double samletDosis() {
+        double dagligDosis = 0;
+
+        for (Dosis dosis : doser) {
+            dagligDosis += dosis.getAntal();
+        }
+
+        return dagligDosis * antalDage();
+    }
+
+    @Override
+    public double døgnDosis() {
+        double dagligDosis = 0;
+
+        for (Dosis dosis : doser) {
+            dagligDosis += dosis.getAntal();
+        }
+
+        return dagligDosis;
+    }
+
+    @Override
+    public String getType() {
+        return "Daglig fast";
+    }
 
     public void addDose(int index, Dosis dosis) {
         if (index >= 0 && index < 4) {
