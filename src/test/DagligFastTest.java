26f7206226a422e0bf810a07b5a21ccf0f514606
TrashHologram <makkermark@gmail.com>
2024-10-04 10:23:34 +0200

    lille rettelse i DagligFastTest

--- src/test/DagligFastTest.java
+++ src/test/DagligFastTest.java
@@ -13,13 +13,13 @@ class DagligFastTest {
 
     //Test af samletDosis()
     @Test
-    void samletDosisEnDag() {
+    public void samletDosisEnDag() {
         DagligFast o = new DagligFast(LocalDate.of(2024, 10, 03), LocalDate.of(2024, 10, 03), 1, 1, 1, 1);
         double samletDosis = o.samletDosis();
         assertEquals(samletDosis, 4);
     }
     @Test
-    void samletDosistreDage() {
+    public void samletDosistreDage() {
         DagligFast o = new DagligFast(LocalDate.of(2024, 10, 03), LocalDate.of(2024, 10, 10), 1, 2, 3, 4);
         double samletDosis = o.samletDosis();
         assertEquals(samletDosis, 80);
@@ -27,7 +27,7 @@ class DagligFastTest {
 
     //Test af døgnDosis()
     @Test
-    void døgnDosis() {
+    public void døgnDosis() {
         DagligFast o = new DagligFast(LocalDate.of(2024, 10, 03), LocalDate.of(2024, 12, 03), 1, 2, 3, 4);
         double døgnDosis = o.døgnDosis();
         assertEquals(døgnDosis, 10);
