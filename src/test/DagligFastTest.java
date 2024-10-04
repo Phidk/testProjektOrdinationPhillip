dac394ded2eb86d3718f9241f93e868f70623561
TrashHologram <makkermark@gmail.com>
2024-10-04 11:19:12 +0200

    Tilføjet testcase numre

--- src/test/DagligFastTest.java
+++ src/test/DagligFastTest.java
@@ -12,22 +12,25 @@ import java.time.LocalDate;
 class DagligFastTest {
 
     //Test af samletDosis()
+    //TC1
     @Test
-    public void samletDosisEnDag() {
+    public void testSamletDosisEnDag() {
         DagligFast o = new DagligFast(LocalDate.of(2024, 10, 03), LocalDate.of(2024, 10, 03), 1, 1, 1, 1);
         double samletDosis = o.samletDosis();
         assertEquals(samletDosis, 4);
     }
+    //TC2
     @Test
-    public void samletDosistreDage() {
+    public void testSamletDosistreDage() {
         DagligFast o = new DagligFast(LocalDate.of(2024, 10, 03), LocalDate.of(2024, 10, 10), 1, 2, 3, 4);
         double samletDosis = o.samletDosis();
         assertEquals(samletDosis, 80);
     }
 
     //Test af døgnDosis()
+    //TC3
     @Test
-    public void døgnDosis() {
+    public void testDøgnDosis() {
         DagligFast o = new DagligFast(LocalDate.of(2024, 10, 03), LocalDate.of(2024, 12, 03), 1, 2, 3, 4);
         double døgnDosis = o.døgnDosis();
         assertEquals(døgnDosis, 10);
